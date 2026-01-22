95aaf41bf0eb6975d164197b2ae86d50aab06172
Adil Rahman <232363244+adil-rahman-mt@users.noreply.github.com>
2026-01-22 23:03:46 +0000

    Use url to connect to DB

--- database.py
+++ database.py
@@ -1,12 +1,7 @@
-from peewee import PostgresqlDatabase
 import os
 from dotenv import load_dotenv
+from playhouse.db_url import connect
 
 load_dotenv()
 
-db = PostgresqlDatabase(os.getenv('DB_NAME'),
-                           host=os.getenv('DB_HOST'),
-                           user=os.getenv('DB_USERNAME'),
-                           password=os.getenv('DB_PASSWORD'),
-                           port=25060,
-                           autoconnect=False)
+db = connect(os.environ.get('DB_URL'))
