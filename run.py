8c85f550cc8011966cc13143ecdef2f6e3608111
Adil Rahman <232363244+adil-rahman-mt@users.noreply.github.com>
2026-02-03 12:13:32 +0000

    Add link to repo in home route for documentation

--- run.py
+++ run.py
@@ -7,4 +7,8 @@ if __name__ == "__main__":
 
 @app.route('/')
 def home():
-    return 'Welcome to Endgame: Phase 1'
+    return """
+    <p>Welcome to Endgame: Phase 1.</p>
+    <p>For the API documentation, click <a href="https://github.com/adil-rahman-mt/endgame-phase-1">here</a></p>
+    <p>Or visit https://github.com/adil-rahman-mt/endgame-phase-1</p>
+    """
